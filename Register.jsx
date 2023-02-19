8979e254e15672bd0197736e39c67e6f0012c9e0
Raefabdulnazir <raefnazir@gmail.com>
2023-02-19 21:20:43 +0530

    Login Page created

---
+++ Register.jsx
@@ -0,0 +1,68 @@
+
+import React, { useState } from "react";
+
+export const Register = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+    const [mobileNumber, setMobileNumber] = useState('');
+    const [vehicleModel, setVehicleModel] = useState("");
+    const [vehicleNumber, setVehicleNumber] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+    
+    const handleMobileNumberChange = (event) => {
+        setMobileNumber(event.target.value);
+      };
+    
+    const handleVehicleModelChange = (event) => {
+        setVehicleModel(event.target.value);
+      };
+    
+    const handleVehicleNumberChange = (event) => {
+        setVehicleNumber(event.target.value);
+      };
+
+    return (
+        <div className="auth-form-container">
+            <h2>Register</h2>
+        <form className="register-form" onSubmit={handleSubmit}>
+            <label htmlFor="name">Full name</label>
+            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Enter your Full Name" />
+               
+                
+                
+            <label htmlFor="mobileNumber">Mobile Number:</label>
+            <input type="tel" id="mobileNumber" name="mobileNumber" value={mobileNumber} placeholder="Enter your Mobile Number" onChange={handleMobileNumberChange}/>
+                
+               
+                
+            <label htmlFor="vehicleModel">Vehicle Model:</label>
+            <input type="text" id="vehicleModel" name="vehicleModel" placeholder="Enter your Vehicle Model" value={vehicleModel} onChange={handleVehicleModelChange}/>
+                
+            
+                
+            <label htmlFor="vehicleNumber">Vehicle Number:</label>
+            <input type="text" id="vehicleNumber" name="vehicleNumber" placeholder="Enter your Vehicle Number" value={vehicleNumber} onChange={handleVehicleNumberChange}/>
+                
+                
+                
+            <label htmlFor="email">Email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Enter E-mail address" id="email" name="email" />
+            
+                
+                
+            <label htmlFor="password">Password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Enter password" id="password" name="password" />
+                
+                
+            <button type="submit">Register</button>
+            
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+    </div>
+    )
+}
